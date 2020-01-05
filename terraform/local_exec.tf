e71be8a812e90596be8227a0f223760d8d24dede
Radhika-cloud <55882387+Radhika-cloud@users.noreply.github.com>
2020-01-05 17:09:12 +0530

    Update local_exec.tf

--- terraform/local_exec.tf
+++ terraform/local_exec.tf
@@ -8,19 +8,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# resource "aws_instance" "backend" {
-#   ami                    = "ami-04763b3055de4860b"
-#   instance_type          = "t2.micro"
-#   key_name               = "${var.key_name}"
-#   vpc_security_group_ids = ["${var.sg-id}"]
-# }
-
 resource "aws_instance" "backend" {
-  ami                    = "ami-04763b3055de4860b"
+  ami                    = "ami-0dbf5ea29a7fc7e05"
   instance_type          = "t2.micro"
-  key_name               = "Terraform_demo"
-  vpc_security_group_ids = ["sg-075ca3a5467ab6a3e"]
+  key_name               = "${var.key_name}"
+  vpc_security_group_ids = ["${var.sg-id}"]
 }
+
+
 
 resource "null_resource" "remote-exec-1" {
     connection {
